95eb69e17ca5f427bc78b63a1c01d5c505d9f730
Pedro <pedro.vcosta405@gmail.com>
2021-06-08 10:25:16 -0300

    [ADD] Operador aritmético

--- js/operadoresAritmeticos.js
+++ js/operadoresAritmeticos.js
@@ -1,13 +1,24 @@
 
-var subtotal = (13 + 1) * 5;
-var frete = 0.5 * (13 + 1);
-var total = subtotal + frete;
+subtotal = (13 + 1) * 5;
+frete = 0.5 * (13 + 1);
+total = subtotal + frete;
 
-var subResultado = document.getElementById('subtotalvalor');
-subResultado.textContent = subtotal;
+cifrao = "R$ ";
 
-var freteResultado = document.getElementById('fretevalor');
-freteResultado.textContent = frete;
+subResultado = document.getElementById('subtotalvalor');
+subResultado.textContent += cifrao + subtotal;
 
-var totalResultado = document.getElementById('totalvalor');
-totalResultado.textContent = total;
+
+freteResultado = document.getElementById('fretevalor');
+freteResultado.textContent += cifrao + frete;
+
+totalResultado = document.getElementById('totalvalor');
+totalResultado.textContent += cifrao + total;
+
+hotel = {};
+
+hotel.nome = 'Pedro';
+hotel.quartos = 3;
+resultado2 = document.getElementById('hotel');
+ht = [hotel.nome + ' ' + hotel.quartos]
+resultado2.textContent = ht;
