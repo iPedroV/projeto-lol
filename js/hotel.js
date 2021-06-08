81cfea07c9b3c17fbf13df42c226275249e89d73
Pedro Vitor <pedro.vcosta405@gmail.com>
2021-06-08 17:08:14 -0300

    [UPDATE] Tentativa

--- js/hotel.js
+++ js/hotel.js
@@ -9,29 +9,33 @@ hotel.quartos = 30;
 resultado = document.getElementById('quartoHotel');
 resultado.textContent += hotel.quartos;
 
-hotel.reservados = 20;
+hotel.reservados = 20;//bug
 resultado = document.getElementById('reservaQuartoHotel');
 resultado.textContent += hotel.reservados;
 
 function calcularReserva(quartosReservados) {
-    var reserva =  quartosReservados + 1 ;
-    resultado = document.getElementById('resultado');
+    var i = 0;
+    for (i = 0; i < 10; i++) {
+        var reserva = quartosReservados + 1;//bug
+        
+    }
+    resultado = document.getElementById('reservaQuartoHotel');
     return resultado.textContent = reserva;
 }
 
 resultado = calcularReserva(hotel.reservados);
-resultado = document.getElementById('reservarQuarto')
-
+//resultado = document.getElementById('reservarQuarto')
+/*
 var hotel = {
     nome: 'Vista Park Sul',
     quartos: 40,
     reservados: 25,
-    checaDisponibilidade: function(){
+    checaDisponibilidade: function () {
         return this.quartos + this.reservados;
     }
 }
 
+*/
 
-
-// document.write('aalooo '+ resultado);
+document.write('aalooo '+ reserva);
 
